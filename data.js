602c17217dcca79f68383393dd6f73237f72dd3e
nh5805844-gif <nh5805844@gmail.com>
2026-01-01 00:46:46 +0700

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+const data_name = [
+    {
+        ma_ten: "cong",
+        name: "Hoàng",
+        message: "Hello, I'm Hoàng",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    },
+    {
+        ma_ten: "thanh",
+        name: "Nguyễn",
+        message: "Hello, I'm Nguyễn",
+        img: "https://vcdn1-giaitri.vnecdn.net/2014/11/12/12-1415753927.jpg?w=1200&h=0&q=100&dpr=1&fit=crop&s=eeNdzX-KP7izXlmIm_xMCg"
+    }
+]
